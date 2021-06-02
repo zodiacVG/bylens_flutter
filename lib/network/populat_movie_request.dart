eef004a32d7fe4a03df7f7c9cedf8496a7278d8f
zodiacVG <zhangzibo418@outlook.com>
2021-06-02 21:24:44 +0800

    使用http成功了一小会儿又不行了

--- lib/network/populat_movie_request.dart
+++ lib/network/populat_movie_request.dart
@@ -1,29 +1,25 @@
-import 'dart:convert';
+import 'dart:convert' as convert;
 import 'package:bylens/model/popular_model.dart';
-import 'dart:io';
 import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 
 class PopularRequest {
   Future<dynamic> getMoviePopularList(int page) async { //todo 格式原来是List<popularTmdb>
-    String result;
-    var httpClient = new HttpClient();
+    var result;
     print('开始');
     // 1.拼接URL
     final url = "https://api.themoviedb.org/3/movie/popular?api_key=3c4cb870e3f3c729ef1eb2d0538ba4f7&language=en-US?page=$page";
-
     // 2.发送请求
     try{
-      final request = await httpClient.getUrl(Uri.parse(url));
-      print(request);
-      var response=await request.close();
+      var response = await http.get(Uri.parse(url),headers: {"Accept": "application/json"});
       print(response);
-      if (response.statusCode == HttpStatus.OK) {
+      if (response.statusCode == 200) {
         print('代码ok');
-        var json = await response.transform(utf8.decoder).join();
-        var data = jsonDecode(json);
-        result = data.page.results; //不确定是不是这样
+        var json = convert.jsonDecode(response.body) as Map<String, dynamic>;
+        print('json is:');
+        print(json);
+        result = json['results']; //不确定是不是这样
       } else {
         print('不太行');
         result =
@@ -33,13 +29,13 @@ class PopularRequest {
       print('e出错');
       print(e);
     }
-
     // 3.转成模型对象
-    // final subjects = result;
-    // List<popularTmdb> movies = [];
-    // for (var sub in subjects) {
-    //   movies.add(popularTmdb.fromJson(sub));
-    // }
+    final subjects = result;
+    List<popularTmdb> movies = [];
+    for (var sub in subjects) {
+      movies.add(popularTmdb.fromJson(sub));
+    }
+    print(movies);
     //todo 这里为了测试返回了result
     return result;
   }
